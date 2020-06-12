3412b3318b7e8a3c297029f56afd1227e30dbf65
Chris Subagio <33039110+amzn-chris-subagio@users.noreply.github.com>
2020-06-12 14:59:29 -0700

    chore: bumping dependencies, fixing build issues
    * bumped several packages to ~May 2020 versions to correct for auditing warnings
    * removed unused cson-parser
    * fixed first time install order issue with rollup plugins
    * includes update to more efficient version of file watcher chokidar
    * switching lame package to @suldashi/lame with the node 10+ build patch that hasn't landed in the main (possibly abandoned) package yet.
    * changing over the use of the Buffer() constructor to the explicit .from() function as the Buffer(buffer) copy constructor version is being deprecated eventually
    * refactored nyc directory merging code to account for subdirectories

--- cacheCoverage.js
+++ cacheCoverage.js
@@ -7,21 +7,34 @@
 
 // Script to cache the current coverage report. This can be used to persist the reports in a
 // .nyc_output_combined directory, which is then utilized by npm run coverage:report.
-const execSync = require('child_process').execSync;
 const path = require('path');
-const isWin = (process.platform === "win32");
+const mkdirp = require('mkdirp');
+const move = require('move-concurrently')
+const fs = require('fs');
 
-let src = path.join(`${__dirname}`, '.nyc_output', '*');
-let dst = path.join(`${__dirname}`, '.nyc_output_combined');
-let cmd = '';
+let src = path.join(__dirname, '.nyc_output');
+let dst = path.join(__dirname, '.nyc_output_combined');
 
-if (isWin) {
-  cmd = `if not exists "${dst}" mkdir "${dst}" && move ${src} ${dst}`;
-} else {
-  cmd = `mkdir -p ${dst} && mv ${src} ${dst}`;
+let promises = [];
+let moveDir = (leaf) => {
+  let things = fs.readdirSync(path.join(src,leaf));
+  mkdirp.sync(path.join(dst,leaf));
+  for ( let i=0; i<things.length; ++i ) {
+    let thing = things[i];
+    if ( thing[0] == '.' ) { continue; }
+    let stat = fs.lstatSync(path.join(src,leaf,thing));
+    if ( stat.isFile() ) {
+      promises.push( move( path.join(src,leaf,thing), path.join(dst,leaf,thing) ) );
+    } else if ( stat.isDirectory() ) {
+      moveDir(path.join(leaf,thing));
+    }
+  }
 }
+moveDir('');
 
-execSync(cmd, {
-  encoding: 'utf-8',
-  stdio: 'inherit'
+Promise.all(promises).then( () => {
+  console.log("completed coverage file move");
+}).catch( (err) => {
+  console.error(`failed to move coverage files: ${err}`);
+  process.exit(-1);
 });
